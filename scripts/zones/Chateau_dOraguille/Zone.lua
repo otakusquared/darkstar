7d5eabbe04805695b6160c08034b171479465f24
otakusquared <otakusquared@gmail.com>
2014-11-17 17:36:08 -0600

    small fixes to sandy mission
    Signed-off-by: otakusquared <otakusquared@gmail.com>

--- scripts/zones/Chateau_dOraguille/Zone.lua
+++ scripts/zones/Chateau_dOraguille/Zone.lua
@@ -23,14 +23,15 @@ end;
 -----------------------------------		
 
 function onZoneIn(player,prevZone)		
-cs = -1;
+
+	local cs = -1;
 	
 	if ((player:getXPos() == 0) and (player:getYPos() == 0) and (player:getZPos() == 0)) then	
 		player:setPos(14.872,8.918,24.002,255);
 	end
 	
 	if(prevZone == 231 and player:getCurrentMission(SANDORIA) == THE_CRYSTAL_SPRING and player:getVar("MissionStatus") == 2) then	
-		cs = 0x022b;
+		cs = 0x022B;
 	elseif(prevZone == 231 and player:hasKeyItem(MESSAGE_TO_JEUNO_SANDORIA)) then
 		cs = 0x01FD;
 	elseif(player:getVar("SecretWeaponStatus") == 1) then
@@ -40,8 +41,7 @@ cs = -1;
 	end
 	
 	return cs;
-	
-end;		
+end;
 
 -----------------------------------		
 -- onConquestUpdate		
@@ -79,15 +79,15 @@ function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);	
 --printf("RESULT: %u",option);	
 	
-	if(csid == 0x022b) then	
+	if(csid == 0x022B) then
 		player:setVar("MissionStatus",3);
 	elseif(csid == 0x01FD) then
 		player:setVar("MissionStatus",9);
 		player:delKeyItem(MESSAGE_TO_JEUNO_SANDORIA);
-	elseif(csid == 0) then
-		player:setVar("SecretWeaponStatus",2)
+	elseif(csid == 0x0000) then
+		player:setVar("SecretWeaponStatus",2);
 	elseif(csid == 0x0074) then
-		player:setVar("MissionStatus",1)
+		player:setVar("MissionStatus",1);
 	end
 	
 end;		
